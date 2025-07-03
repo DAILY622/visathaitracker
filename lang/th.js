4dca037225ca264edaa03f571240385fb3c9c981
DAILY622 <kingsleyotisi46@gmail.com>
2025-07-03 17:35:42 +0100

    Create lang folder and add Thai language file

---
+++ lang/th.js
@@ -0,0 +1,12 @@
+export default {
+  welcome: "ยินดีต้อนรับ",
+  login: "เข้าสู่ระบบ",
+  logout: "ออกจากระบบ",
+  profile: "โปรไฟล์",
+  submit: "ส่งข้อมูล",
+  cancel: "ยกเลิก",
+  quests: "เควส",
+  rewards: "รางวัล",
+  leaderboard: "กระดานผู้นำ",
+  settings: "การตั้งค่า"
+};
