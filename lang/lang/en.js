c4769d038af32ac820fb4ff98b1b989056d0583e
DAILY622 <kingsleyotisi46@gmail.com>
2025-07-03 17:38:24 +0100

    Add English language file (en.js)

---
+++ lang/lang/en.js
@@ -0,0 +1,12 @@
+export default {
+  welcome: "Welcome",
+  login: "Login",
+  logout: "Logout",
+  profile: "Profile",
+  submit: "Submit",
+  cancel: "Cancel",
+  quests: "Quests",
+  rewards: "Rewards",
+  leaderboard: "Leaderboard",
+  settings: "Settings"
+};
